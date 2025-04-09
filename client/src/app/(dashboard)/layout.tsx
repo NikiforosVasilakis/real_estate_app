6bd3d7624e12c6ee99e1c936079f393e93078175
NikiforosVasilakis <nikiforos.vasilakis@gmail.com>
2025-04-09 03:38:31 +0300

    started the sidebar

--- client/src/app/(dashboard)/layout.tsx
+++ client/src/app/(dashboard)/layout.tsx
@@ -1,21 +1,29 @@
+"use client"
+
 import Navbar from '@/components/Navbar';
-import { Sidebar, SidebarProvider } from '@/components/ui/sidebar';
+import { SidebarProvider } from '@/components/ui/sidebar';
 import { NAVBAR_HEIGHT } from '@/lib/constants';
+import Sidebar from '@/components/AppSidebar';
 import React from 'react'
+import { useGetAuthUserQuery } from '@/state/api';
 
 const DashboardLayout = ({children}:{children: React.ReactNode}) => {
+  const {data: authUser} = useGetAuthUserQuery();
+
+  if (!authUser?.userRole) return null;
+
   return (
     <SidebarProvider>
         <div className='min-h-screen w-full bg-primary-100'>
-        <Navbar />
-        <div style={{paddingTop: `${NAVBAR_HEIGHT}px`}}>
-            <main className='flex'>
-                <Sidebar />
-                <div className='flex-grow transition-all duration-300'>
-                    {children}
-                </div>
-            </main>
-        </div>
+          <Navbar />
+          <div style={{paddingTop: `${NAVBAR_HEIGHT}px`}}>
+              <main className='flex'>
+                  <Sidebar userType={authUser.userRole.toLowerCase()} />
+                  <div className='flex-grow transition-all duration-300'>
+                      {children}
+                  </div>
+              </main>
+          </div>
         </div>
     </SidebarProvider>
   )
--- client/src/components/AppSidebar.tsx
+++ client/src/components/AppSidebar.tsx
@@ -1,10 +1,57 @@
 import React from "react";
+import { usePathname } from "next/navigation";
+import { SidebarHeader, SidebarMenu, SidebarMenuItem, useSidebar, Sidebar } from "./ui/sidebar";
+import { Building, FileText, Heart, Home, Menu, Settings, X } from "lucide-react";
+import { NAVBAR_HEIGHT } from "@/lib/constants";
+import { cn } from "@/lib/utils";
+
 
 const AppSidebar = ({userType}: AppSidebarProps) =>{
-    return(
-        <div>
+    const pathname = usePathname();
+    const {toggleSidebar, open} = useSidebar();
 
-        </div>
+    const navlinks =
+        userType === "manager"
+        ? [
+            [{icon: Building, laber: "Properties", href:"/managers/properties"},
+             {icon: FileText, laber: "Applications", href:"/managers/applications"},
+             {icon: Settings, laber: "Settings", href:"/managers/settings"},
+            ]
+        ] : 
+        [
+            {icon: Heart, laber: "Favorites", href:"/tenants/favorites"},
+            {icon: Home, laber: "Residences", href:"/tenants/residences"},
+            {icon: Settings, laber: "Settings", href:"/tenants/settings"},
+        ];
+    
+    return(
+        <Sidebar collapsible="icon" className="fixed left-0 bg-white shadow-lg" style={{top: `${NAVBAR_HEIGHT}px`, height: `calc(100vh - ${NAVBAR_HEIGHT}px)`}}>
+            <SidebarHeader>
+                <SidebarMenu>
+                    <SidebarMenuItem>
+                        <div className={cn(
+                            "flex min-h-[56px] w-full items-center pt-3 mb-3", open ? "justify-between px-6" : "justify-center"
+                        )}>
+                            {open ? (
+                                <>
+                                    <h1 className="text-xl font-bold text-gray-800">
+                                        {userType==="manager"? "manager View": "Renter view"}
+                                    </h1>
+                                    <button className="hover:bg-gray-100 p-2 rounded-md" onClick={()=>toggleSidebar()}>
+                                        <X className="h-6 w-6 text-gray-600" />
+                                    </button>
+                                </>
+                            ):
+                            (
+                                <button className="hover:bg-gray-100 p-2 rounded-md" onClick={()=>toggleSidebar()}>
+                                    <Menu className="h-6 w-6 text-gray-600" />
+                                </button>
+                            )}
+                        </div>
+                    </SidebarMenuItem>
+                </SidebarMenu>
+            </SidebarHeader>
+        </Sidebar>
     )
 }
 
